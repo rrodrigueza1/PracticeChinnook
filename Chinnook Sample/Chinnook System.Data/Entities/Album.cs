00c6ce627f5218d4f38e2c1152226f2b718bce59
rrodrigueza1 <rrodrigueza1@nait.ca>
2016-09-26 16:19:50 -0600

    Chinnook Poco and Bogo

---
+++ Chinnook Sample/Chinnook System.Data/Entities/Album.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+#endregion
+
+namespace Chinnook_System.Data.Entities
+{
+    [Table("Artists", Schema = "dbo")]
+    public class Album
+    {
+        public int AlbumId { get; set; }
+        public string Title { get; set; }
+        public int ArtistId {get; set;}
+        public int ReleaseYear { get; set; }
+        public string ReleaseLabel { get; set; }
+    }
+}
---
+++ Chinnook Sample/Chinnook System.Data/Entities/Artist.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+#endregion
+
+namespace Chinnook_System.Data.Entities
+{
+    //point to the sql table that this file maps
+    [Table("Artists", Schema = "dbo")]
+    public class Artist
+    {
+        //   "Key notations is optional if the sql pkey ends in ID or id"
+        //     required if the default of entity is not identity
+        //     required if pkey is compound
+
+        // properties can be fully implemented or auto implemented
+        // poperty names should use sql attribute name
+        // properties should listed in the same order as sql table attributes for ease of maintenance
+        [Key]
+
+        public int ArtistsId { get; set; }
+        public string Name { get; set; }
+        
+    }
+}
