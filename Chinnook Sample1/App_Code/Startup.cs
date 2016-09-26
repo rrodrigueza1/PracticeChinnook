7874c572b52f5476ea22ea93b98142765969ab3d
rrodrigueza1 <rrodrigueza1@nait.ca>
2016-09-26 15:37:18 -0600

    Create Website for Chinnook Sample

---
+++ Chinnook Sample1/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Chinnook_Sample.Startup))]
+namespace Chinnook_Sample
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
