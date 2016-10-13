0fc2959641f8354e31483fdcc0c60c7fac3cd22d
rrodrigueza1 <rrodrigueza1@nait.ca>
2016-10-13 16:23:13 -0600

    Create RoleManager Class and Perform set-up

---
+++ Chinnook Sample/Chinnook System/Security/RoleManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using Microsoft.AspNet.Identity; // Getting Role Manager
+using Microsoft.AspNet.Identity.EntityFramework; // Getting IdentityRole, RoleStore
+#endregion
+
+namespace Chinnook_System.Security
+{
+    public class RoleManager : RoleManager<IdentityRole>
+    {
+        public  RoleManager() : base (new RoleStore<IdentityRole>(new ApplicationDbContext()))
+        {
+
+        }
+    }
+}
